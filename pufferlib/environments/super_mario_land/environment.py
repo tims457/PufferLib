2ddeef2ff7d5d3a1509c29f6ab39d928bb15730e
Tim <6698857+tims457@users.noreply.github.com>
2025-05-03 15:09:14 -0600

    initial super mario land files

---
+++ pufferlib/environments/super_mario_land/environment.py
@@ -0,0 +1,31 @@
+from pdb import set_trace as T
+
+import gymnasium
+import functools
+
+from super_mario_land_rl.mario_env import SuperMarioLandEnv
+
+import pufferlib.emulation
+import pufferlib.postprocess
+
+
+def env_creator(name='super_mario_land'):
+    return functools.partial(make, name)
+
+def make(name, headless: bool = True, state_path=None, buf=None):
+    '''Super Mario Land'''
+    env = SuperMarioLandEnv()
+    env = RenderWrapper(env)
+    env = pufferlib.postprocess.EpisodeStats(env)
+    return pufferlib.emulation.GymnasiumPufferEnv(env=env, buf=buf)
+
+class RenderWrapper(gymnasium.Wrapper):
+    def __init__(self, env):
+        self.env = env
+
+    @property
+    def render_mode(self):
+        return 'rgb_array'
+
+    def render(self):
+        return self.env.screen.screen_ndarray()
